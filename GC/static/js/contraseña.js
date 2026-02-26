e1d347bd3ed767c14a1750d0874c569b2b4d550e
Diego7056 <diegoprogramacion62@gmail.com>
2026-02-26 10:45:33 -0500

    Actualizacion en contrseña.js

--- GC/static/js/contraseña.js
+++ GC/static/js/contraseña.js
@@ -18,3 +18,19 @@ document.addEventListener("DOMContentLoaded", function () {
             });
         });
         });
+
+    document.getElementById('cambiarContraseñaModal').addEventListener('hidden.bs.modal', function () {
+    // Limpiar campos
+    document.getElementById('cambio-contraseña-form').reset();
+    
+    // Resetear todos los ojos a "cerrado"
+    this.querySelectorAll('.edit-icon i').forEach(function(icono) {
+        icono.classList.remove('fa-eye-slash');
+        icono.classList.add('fa-eye');
+    });
+
+    // Volver todos los inputs a tipo password
+    this.querySelectorAll('input[type="text"]').forEach(function(input) {
+        input.type = 'password';
+    });
+});
